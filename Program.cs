f9117b55a48064ed79fb3c73d561c272ae35142c
pRaV33N-S <praveen.srinivasan1@mphasis.com>
2023-07-12 14:48:59 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryParseConvert
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word;
+            Console.WriteLine("Enter the String");
+            word=Console.ReadLine();
+            try
+            {
+                /*int tryParsedNum;
+                bool flag=int.TryParse(word,out tryParsedNum);
+                Console.WriteLine($"The number is {tryParsedNum}");
+                float parsedNum = float.Parse(word);
+                Console.WriteLine($"The number is {parsedNum}"); */
+                double convertedNum = Convert.ToDouble(word);
+                Console.WriteLine($"The number is {convertedNum}");
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error!!!"+ex.Message);
+            }
+            
+        }
+    }
+}
